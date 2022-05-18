6a893191b9b955727d9f90497b511f1b41e14f97
Ian <Ian@MacBookde-MacBook-Pro.local>
2022-05-18 16:16:53 -0400

    computes VC_dimension and updated kendall functions
    currently correlation_function will always be NaN because there is no change in width/depth of the network, which is what changes VC dimension

--- kendall_correlation.py
+++ kendall_correlation.py
@@ -1,10 +1,11 @@
 from __future__ import print_function
-import nn
+import nn as nnn
 import argparse
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import numpy
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 from torchsummary import summary
@@ -17,11 +18,15 @@ def correlation_function(comp_measure_list, gen_gap_list):
     comp_measure_list: list of μ(θ) for each hyperparameter set θ
     gen_gap_list: list of  g(θ) for each hyperparameter set θ 
     """
-    kendall, _ = kendalltau(comp_measure_list, gen_gap_list)
-    return kendall
+    n = len(comp_measure_list)
+    cardinality = n**2 - n
+    running_sum = 0
+    for i,j in zip(n,n):
+        if i != j:
+            running_sum += numpy.sign(comp_measure_list[i] - comp_measure_list[j])*numpy.sign(gen_gap_list[i] - gen_gap_list[j])
+    return running_sum/n
 
-#to do
-def basic_kendall(w, bs, lr, epochs, dp, d, op):
+def basic_kendall(bs, lr, epochs, dp, measure='VC', lst='True'):
     """
     Computes the vanilla kendall correlation on a hyperparameter space choices Θ_1 x ... Θ_n. 
 
@@ -34,19 +39,37 @@ def basic_kendall(w, bs, lr, epochs, dp, d, op):
     w: list of network layer widths  
     d: list of network depth choices
     op: list of optimizer choices
+    measure: either 'VC' or 'norm' (complexity measure to use)
+    lst: boolean indicating whether to return a list of VC dimensions or the kendall correlation number
     """
-    model_list, train_loss_list, test_loss_list = nn.get_models(bs, lr, epochs, dp)
+    model_list, train_loss_list, test_loss_list = nnn.get_models(bs, lr, epochs, dp)
     gen_gap_list = [train-test for train, test in zip(train_loss_list, test_loss_list)]
-    # todo: generate comp_measure_list from a list of models. 
+    if measure == 'VC':
+        vc_list = VC_dimension(model_list)
+        return vc_list if lst else correlation_function(vc_list, gen_gap_list)
+    else:
+        return correlation_function(network_norm(model_list), gen_gap_list)
+    
 
-#to do
-def VC_dimension():
+def VC_dimension(model_list):
     """
-    Computes the VC dimension of a neural network 
-    """
+    Computes the VC dimension of each network model in model_list
 
-#to do
-def network_norm():
+    Tightest VC Dimension for a regular nn is O(WLlog(W)), where W = # of weights, L = # of layers
+
+    model_list: a list of neural network models generated from hyperparameter space
     """
-    Computes the spectral norm of a neural network
+    vc_list = []
+    for model in model_list:
+        vc_list.append(nnn.get_vc(model))
+    return vc_list
+
+#probably to do; kinda a stub rn
+def network_norm(model_list):
     """
+    Computes the spectral norm of each network model in model_list
+    """
+    norm_list = []
+    for model in model_list:
+        norm_list.append(nn.get_weights(model))
+    return norm_list
