d399ed8257eb5f13ed74fe011c1caba2620152ae
Ian <Ian@MacBookde-MacBook-Pro.local>
2022-05-18 16:17:17 -0400

    updated test script

--- experiment.py
+++ experiment.py
@@ -1,5 +1,5 @@
-
 import nn
+import kendall_correlation
 
 # hp = {"batch_size":64, "lr":1}
 # model, train_loss, test_loss = nn.get_model(hp, epochs=1)
@@ -7,13 +7,18 @@ import nn
 # print('Test Loss: %.3f'%(test_loss))
 # print(nn.get_weights(model))
 
-bs = [128]
-dp = [0.25]
+bs = [64, 128]
 lr = [1]
 epochs = [1]
+dp = [0.25, 0.5]
 depth = [1,2]
-width = [8,16]
-model_list, train_loss_list, test_loss_list = nn.get_models(bs, lr, epochs, dp)
-print('# of Models:' + str(len(model_list)))
-print(train_loss_list)
-print(test_loss_list)
+width = [8]
+# model_list, train_loss_list, test_loss_list = nn.get_models(bs, lr, epochs, dp)
+vc_list = kendall_correlation.basic_kendall(bs, lr, epochs, dp, measure='VC', lst=True)
+# VC_ken_corr = kendall_correlation.basic_kendall(bs, lr, epochs, dp, measure='VC', lst=False) -- NaN atm cuz there's no change in width/depth
+
+# print('# of Models:' + str(len(model_list)))
+# print('Training Loss:' + str(train_loss_list))
+# print('Testing Loss:' +str(test_loss_list))
+print('VC_list:' + str(vc_list))
+# print('correlation' + str(VC_ken_corr))
