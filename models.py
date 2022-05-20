ad850cbd19f5636e792bc4880b62eee40d5ad6b5
Winnie Chow <wc459@cornell.edu>
2022-05-20 15:53:21 +0800

    Formatting

--- models.py
+++ models.py
@@ -11,46 +11,46 @@ from sklearn.model_selection import ParameterGrid
 from math import log10
 import numpy as np
 
-transform=transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))
-    # values from https://github.com/kuangliu/pytorch-cifar/issues/19
-    ])
-dataset1 = datasets.CIFAR10('../data', train=True, download=True,
-    transform=transform)
-dataset2 = datasets.CIFAR10('../data', train=False,
-    transform=transform)
+# transform=transforms.Compose([
+#     transforms.ToTensor(),
+#     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))
+#     # values from https://github.com/kuangliu/pytorch-cifar/issues/19
+#     ])
+# dataset1 = datasets.CIFAR10('../data', train=True, download=True,
+#     transform=transform)
+# dataset2 = datasets.CIFAR10('../data', train=False,
+#     transform=transform)
 
-#input_size = 32 = W
-#kernel_size = 3 = K
-#padding - 0 = P
-#stride = 1 = S
-#(W-K+2P)/S + 1 -->  
-class Net(nn.Module):
-    def __init__(self, dp):
-        #I decreased the channel numbers significantly for faster debugging.
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 3, 3, 1)
-        self.conv2 = nn.Conv2d(3, 3, 3, 1)
-        self.dropout1 = nn.Dropout(dp)
-        self.dropout2 = nn.Dropout(dp)
-        self.fc1 = nn.Linear(588, 20)  # 3 x 14 x 14
-        self.fc2 = nn.Linear(20, 10)
+# #input_size = 32 = W
+# #kernel_size = 3 = K
+# #padding - 0 = P
+# #stride = 1 = S
+# #(W-K+2P)/S + 1 -->
+# class Net(nn.Module):
+#     def __init__(self, dp):
+#         #I decreased the channel numbers significantly for faster debugging.
+#         super(Net, self).__init__()
+#         self.conv1 = nn.Conv2d(3, 3, 3, 1)
+#         self.conv2 = nn.Conv2d(3, 3, 3, 1)
+#         self.dropout1 = nn.Dropout(dp)
+#         self.dropout2 = nn.Dropout(dp)
+#         self.fc1 = nn.Linear(588, 20)  # 3 x 14 x 14
+#         self.fc2 = nn.Linear(20, 10)
 
-    def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x, 2)
-        x = self.dropout1(x)
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.dropout2(x)
-        x = self.fc2(x)
-        output = F.log_softmax(x, dim=1)
-        return output
+#     def forward(self, x):
+#         x = self.conv1(x)
+#         x = F.relu(x)
+#         x = self.conv2(x)
+#         x = F.relu(x)
+#         x = F.max_pool2d(x, 2)
+#         x = self.dropout1(x)
+#         x = torch.flatten(x, 1)
+#         x = self.fc1(x)
+#         x = F.relu(x)
+#         x = self.dropout2(x)
+#         x = self.fc2(x)
+#         output = F.log_softmax(x, dim=1)
+#         return output
 
 
 def train(model, device, train_loader, optimizer, epoch, log_interval, dry_run):
@@ -76,7 +76,7 @@ def train(model, device, train_loader, optimizer, epoch, log_interval, dry_run):
             )
             if dry_run:
                 break
-    train_loss=running_loss/len(train_loader)
+    train_loss = running_loss / len(train_loader)
     return train_loss
 
 
@@ -88,52 +88,68 @@ def test(model, device, test_loader):
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            test_loss += F.nll_loss(
+                output, target, reduction="sum"
+            ).item()  # sum up batch loss
+            pred = output.argmax(
+                dim=1, keepdim=True
+            )  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
 
     test_loss /= len(test_loader.dataset)
 
     return test_loss
 
+
 def make_nn(width, depth, dropout):
-    ''' Return network in network model with given width, depth and dropout'''
+    """Return network in network model with given width, depth and dropout"""
 
     def nin_block(in_channels, out_channels, kernel_size, strides, dropout):
         return nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size, strides),
             nn.ReLU(),
-            nn.Conv2d(out_channels, out_channels, 1, 1), nn.ReLU(),
-            nn.Conv2d(out_channels, out_channels, 1, 1), nn.ReLU(),
-            nn.Dropout(dropout))
-        
+            nn.Conv2d(out_channels, out_channels, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(out_channels, out_channels, 1, 1),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+        )
+
     def stack_blocks(depth, cin):
         modules = []
         # First 2 blocks
         modules.append(nin_block(cin, width, kernel_size=3, strides=2, dropout=dropout))
-        modules.append(nin_block(width, width, kernel_size=3, strides=2, dropout=dropout))
+        modules.append(
+            nin_block(width, width, kernel_size=3, strides=2, dropout=dropout)
+        )
         # More blocks if needed
         i = depth - 2
         while i > 0:
-            modules.append(nin_block(width, width, kernel_size=3, strides=2, dropout=dropout))
-            modules.append(nin_block(width, width, kernel_size=3, strides=2, dropout=dropout))
+            modules.append(
+                nin_block(width, width, kernel_size=3, strides=2, dropout=dropout)
+            )
+            modules.append(
+                nin_block(width, width, kernel_size=3, strides=2, dropout=dropout)
+            )
             i -= 2
-        
+
         return nn.Sequential(*modules)
 
-    cin = 3 # TODO: change for MNIST
+    cin = 3  # TODO: change for MNIST
     net = nn.Sequential(
         stack_blocks(depth, cin),
         # There are 10 label classes
-        nn.Conv2d(width, 10, 1, 1), nn.ReLU(),
+        nn.Conv2d(width, 10, 1, 1),
+        nn.ReLU(),
         nn.AdaptiveAvgPool2d((1, 1)),
         # Transform the four-dimensional output into two-dimensional output with a
         # shape of (batch size, 10)
         nn.Flatten(),
-        nn.LogSoftmax()
-        )
-    
+        nn.LogSoftmax(),
+    )
+
     return net
+
 
 def get_model(
     hp,
@@ -145,9 +161,9 @@ def get_model(
     log_interval=10,
     # save_model=False,
 ):
-    '''
+    """
     hp - dictionary: batch_size, lr
-    '''
+    """
     train_kwargs = {"batch_size": hp["batch_size"]}
     test_kwargs = {"batch_size": test_batch_size}
 
@@ -158,7 +174,7 @@ def get_model(
         cuda_kwargs = {"num_workers": 1, "pin_memory": True, "shuffle": True}
         train_kwargs.update(cuda_kwargs)
         test_kwargs.update(cuda_kwargs)
-        
+
     train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
@@ -171,17 +187,20 @@ def get_model(
     scheduler = StepLR(optimizer, step_size=1, gamma=gamma)
     train_loss = 0
     for hp["epoch"] in range(1, hp["epochs"] + 1):
-        train_loss += train(model, device, train_loader, optimizer, hp["epochs"], log_interval, dry_run)
+        train_loss += train(
+            model, device, train_loader, optimizer, hp["epochs"], log_interval, dry_run
+        )
         scheduler.step()
 
     train_loss /= hp["epochs"]
     test_loss = test(model, device, test_loader)
     return model, train_loss, test_loss
 
+
 def get_models(hp_list, seed=1):
     """
     Given lists of hyperparameters, provide list of all models with their their associated training & testing loss
-    
+
     w: list of network layer widths (tbd)
     d: list of network depth choices (tbd)
     op: list of optimizer choices (tbd)
@@ -189,17 +208,18 @@ def get_models(hp_list, seed=1):
 
     # Load data
     torch.manual_seed(seed)
-    
 
-    transform=transforms.Compose([
+    transform = transforms.Compose(
+        [
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))
             # values from https://github.com/kuangliu/pytorch-cifar/issues/19
-            ])
-    dataset1 = datasets.CIFAR10('../data', train=True, download=True,
-                       transform=transform)
-    dataset2 = datasets.CIFAR10('../data', train=False,
-                       transform=transform)
+        ]
+    )
+    dataset1 = datasets.CIFAR10(
+        "../data", train=True, download=True, transform=transform
+    )
+    dataset2 = datasets.CIFAR10("../data", train=False, transform=transform)
 
     # Get model per each hyperparameter combo
     model_list = []
@@ -214,7 +234,8 @@ def get_models(hp_list, seed=1):
         test_loss_list.append(test_loss)
 
     return model_list, train_loss_list, test_loss_list
-    
+
+
 def get_vc(model):
     """
     Returns VC dimension of a given model
@@ -226,21 +247,23 @@ def get_vc(model):
     L = -1
     for layer in model.children():
         if isinstance(layer, nn.Linear):
-            #for linear activation, w_i = (input_i + 1)*output_i
+            # for linear activation, w_i = (input_i + 1)*output_i
             L += 1
-            fc_weight_num += (layer.in_features+1)*layer.out_features
+            fc_weight_num += (layer.in_features + 1) * layer.out_features
         elif isinstance(layer, nn.Conv2d):
             # in general, # of w_i = (kernel_size^2 * output_{i-1} + 1)*output_{i}, output_i = input_i-1
-            # so total # of weights = \sum_{i} w_{i} 
+            # so total # of weights = \sum_{i} w_{i}
             L += 1
-            nn_weight_num += ((layer.kernel_size[0]**2)*layer.in_channels + 1)*layer.out_channels
-    
+            nn_weight_num += (
+                (layer.kernel_size[0] ** 2) * layer.in_channels + 1
+            ) * layer.out_channels
+
     W = nn_weight_num + fc_weight_num
     print("Weight =" + str(W))
     print("Layer =" + str(L))
-    return W*L*log10(L)
+    return W * L * log10(L)
+
 
 def get_weights(model):
-    ''' Return parameters of the neural network'''
+    """Return parameters of the neural network"""
     return model.fc2.weight
-
