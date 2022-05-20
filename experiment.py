e750b5efe4ee4b29264773e7b31d078f9994929c
Ian <Ian@MacBookde-MacBook-Pro.local>
2022-05-19 20:15:44 -0400

    compactify the tests and added norm complexity tests

--- experiment.py
+++ experiment.py
@@ -13,12 +13,31 @@ epochs = [1]
 dp = [0.25, 0.5]
 depth = [1,2]
 width = [8]
-# model_list, train_loss_list, test_loss_list = nn.get_models(bs, lr, epochs, dp)
-vc_list = kendall_correlation.basic_kendall(bs, lr, epochs, dp, measure='VC', lst=True)
-# VC_ken_corr = kendall_correlation.basic_kendall(bs, lr, epochs, dp, measure='VC', lst=False) -- NaN atm cuz there's no change in width/depth
 
-# print('# of Models:' + str(len(model_list)))
-# print('Training Loss:' + str(train_loss_list))
-# print('Testing Loss:' +str(test_loss_list))
-print('VC_list:' + str(vc_list))
-# print('correlation' + str(VC_ken_corr))
+def test_norm_kendall():
+    # norm_list = kendall_correlation.basic_kendall(bs, lr, epochs, dp, comp_measure='norm', norm_measure='spectral_orig', lst=True)
+    # norm_list2 = kendall_correlation.basic_kendall(bs, lr, epochs, dp, comp_measure='norm', norm_measure='path_norm', lst=True)
+    # norm_list3 = kendall_correlation.basic_kendall(bs, lr, epochs, dp, comp_measure='norm', norm_measure='spec', lst=True)
+    norm_ken_corra = kendall_correlation.basic_kendall(bs, lr, epochs, dp, comp_measure='norm', norm_measure='param_norm', lst=True)
+    norm_ken_corr = kendall_correlation.basic_kendall(bs, lr, epochs, dp, comp_measure='norm', norm_measure='param_norm', lst=False)
+    # print('norm list:' + str(norm_list))
+    # print('norm list2:' + str(norm_list2))
+    # print('norm list3:' + str(norm_list3))
+    print('norm list3:' + str(norm_ken_corra))
+    print('norm kendall: %.3f' % norm_ken_corr)
+
+def test_VC_kendall():
+    vc_list = kendall_correlation.basic_kendall(bs, lr, epochs, dp, comp_measure='VC', lst=True)
+    VC_ken_corr = kendall_correlation.basic_kendall(bs, lr, epochs, dp, comp_measure='VC', lst=False)
+    print('VClist:' + str(vc_list))
+    print('VC kendall: %.3f' % VC_ken_corr)
+
+def test_network():
+    model_list, train_loss_list, test_loss_list = nn.get_models(bs, lr, epochs, dp)
+    print('# of Models: ' + str(len(model_list)))
+    print('Training Loss:' + str(train_loss_list))
+    print('Testing Loss:' +str(test_loss_list))
+    
+test_norm_kendall()
+# test_VC_kendall()
+# test_network()
