2588b84ffae97a87f1f9e0b3167a7f5ce8fb4b51
Ian <Ian@MacBookde-MacBook-Pro.local>
2022-05-19 20:14:16 -0400

    added 4 norm based complexity measures
    from the Google paper

--- kendall_correlation.py
+++ kendall_correlation.py
@@ -1,4 +1,6 @@
 from __future__ import print_function
+from ctypes import resize
+from socketserver import DatagramRequestHandler
 import nn as nnn
 import argparse
 import torch
@@ -21,12 +23,13 @@ def correlation_function(comp_measure_list, gen_gap_list):
     n = len(comp_measure_list)
     cardinality = n**2 - n
     running_sum = 0
-    for i,j in zip(n,n):
-        if i != j:
-            running_sum += numpy.sign(comp_measure_list[i] - comp_measure_list[j])*numpy.sign(gen_gap_list[i] - gen_gap_list[j])
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+                running_sum += numpy.sign(comp_measure_list[i] - comp_measure_list[j])*numpy.sign(gen_gap_list[i] - gen_gap_list[j])
     return running_sum/cardinality
 
-def basic_kendall(bs, lr, epochs, dp, measure='VC', lst='True'):
+def basic_kendall(bs, lr, epochs, dp, comp_measure='VC', norm_measure='param_norm', lst='True'):
     """
     Computes the vanilla kendall correlation on a hyperparameter space choices Θ_1 x ... Θ_n. 
 
@@ -43,12 +46,14 @@ def basic_kendall(bs, lr, epochs, dp, measure='VC', lst='True'):
     lst: boolean indicating whether to return a list of VC dimensions or the kendall correlation number
     """
     model_list, train_loss_list, test_loss_list = nnn.get_models(bs, lr, epochs, dp)
-    gen_gap_list = [train-test for train, test in zip(train_loss_list, test_loss_list)]
-    if measure == 'VC':
+    gen_gap_list = [train_loss_list[i]-test_loss_list[i] for i in range(len(train_loss_list))]
+    print('gen gap list :' + str(gen_gap_list))
+    if comp_measure == 'VC':
         vc_list = VC_dimension(model_list)
         return vc_list if lst else correlation_function(vc_list, gen_gap_list)
     else:
-        return correlation_function(network_norm(model_list), gen_gap_list)
+        norm_list = network_norm(model_list, norm_measure)
+        return norm_list if lst else correlation_function(norm_list, gen_gap_list)
     
 
 def VC_dimension(model_list):
@@ -65,11 +70,48 @@ def VC_dimension(model_list):
     return vc_list
 
 #probably to do; kinda a stub rn
-def network_norm(model_list):
+def network_norm(model_list, norm_measure ='param_norm'):
     """
     Computes the spectral norm of each network model in model_list
+
+    model_list: a list of neural network models generated from hyperparameter space
+    norm_measure : the type of norm to use ('param_norm', 'spectral_orig', 'path_norm', 'spec')
     """
+    #each norm measure is so different that I had to just do them separately to avoid confusion.
+
     norm_list = []
-    for model in model_list:
-        norm_list.append(nn.get_weights(model))
+    
+    if norm_measure == 'param_norm': # formula 42
+        for model in model_list:
+            param_norm = 0
+            for layer in model.children():
+                if not isinstance(layer, nn.Dropout):
+                    param_norm += torch.norm(layer.weight, p='fro')**2
+            norm_list.append(float(param_norm.detach().numpy()))
+    elif norm_measure == 'spectral_orig': # formula 28
+        for model in model_list:
+            first = 1
+            second = 0
+            for layer in model.children():
+                if not isinstance(layer, nn.Dropout):
+                    two_norm = torch.norm(layer.weight, p=2)**2
+                    first *= two_norm
+                    second += (torch.norm(layer.weight, p='fro')**2)/two_norm
+            norm_list.append(float(first.detach().numpy())*float(second.detach().numpy()))
+    elif norm_measure == 'path_norm': # formula 44
+        for model in model_list:
+            path_norm = torch.sum(torch.square(nnn.get_weights(model)))
+            norm_list.append(float(path_norm.detach().numpy()))
+    elif norm_measure == 'spec': # formula 35
+        for model in model_list:
+            depth = -1
+            spec_norm = 1
+            for layer in model.children():
+                if not isinstance(layer, nn.Dropout):
+                    depth+=1
+                    spec_norm = torch.norm(layer.weight, p=2)**2
+            res = float(spec_norm.detach().numpy())
+            norm_list.append(depth*(res**(1/depth)))
     return norm_list
+
+
